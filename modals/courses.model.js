4ecf624eb24b1709b469a32929f75329685f4796
Roshan karki <rohitkc8848@gmail.com>
2023-06-21 02:23:48 +0545

    feat:added the delete handler for the bootcamp

--- modals/courses.model.js
+++ modals/courses.model.js
@@ -44,5 +44,5 @@ const courseSchema=new mongoose.Schema({
 },{timestamps:true})
 
 
-const Course=mongoose.model('Course',courseSchema)
-export default Course
+const CourseModel=mongoose.model('Course',courseSchema)
+export default CourseModel
